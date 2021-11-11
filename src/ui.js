33d5f66fb5c84ccb83cb42fcde14dc188e3c5707
Labnan <khalidmasum@iut-dhaka.edu>
2021-11-11 15:32:37 +0600

    Styling and Implement Copy to Clipbord

--- src/ui.js
+++ src/ui.js
@@ -2,13 +2,22 @@
     const inputTextElement = document.getElementById("input_text");
     const outputElement = document.getElementById("output_text");
     const runButton = document.getElementById("run_button");
+ 
 
     runButton.onclick = ()=>{
         const input = inputTextElement.value;
         if(input.length===0) return;
 
         const matcherRegex = createMatcherRegex(input);
-        outputElement.innerText = matcherRegex;
+        outputElement.value = matcherRegex;
+    }
+
+
+    outputElement.onclick = ()=>{
+        if (outputElement.value.length === 0)
+            return;
+        const output = outputElement.value;
+        navigator.clipboard.writeText(output);
     }
 
 })()
